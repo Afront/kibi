226174fa88fb0cac9b07c72f768a45f83cac4781
Afront <3943720+Afront@users.noreply.github.com>
2020-03-25 23:30:49 +0900

    Add CRLF to every statement

--- src/kibi.cr
+++ src/kibi.cr
@@ -1,15 +1,20 @@
 # TODO: Write documentation for `Kibi`
 module Kibi
   VERSION = "0.1.0"
+  CRLF    = "\r\n"
+
+  def self.put(text)
+    print text.to_s + CRLF
+  end
 
   puts "Welcome!"
 
   STDIN.raw do
     while (input_char = STDIN.read_char) && input_char != 'q'
       if input_char.control?
-        puts input_char.ord
+        put input_char.ord
       else
-        puts "#{input_char.ord} #{input_char}"
+        put "#{input_char.ord} #{input_char}"
       end
     end
   end
