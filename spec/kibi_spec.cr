a9df8f5330112aed7fae6cd1d3b8127b0f032175
Afront <3943720+Afront@users.noreply.github.com>
2020-03-25 22:53:29 +0900

    Init project

---
+++ spec/kibi_spec.cr
@@ -0,0 +1,6 @@
+require "./spec_helper"
+
+describe Kibi do
+  # TODO: Write tests
+
+end
---
+++ src/kibi.cr
@@ -0,0 +1,7 @@
+# TODO: Write documentation for `Kibi`
+module Kibi
+  VERSION = "0.1.0"
+
+  while input = gets(1)
+  end
+end
