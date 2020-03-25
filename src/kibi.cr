7033ff8bc24a13344efe42f10b838cf0a764eae1
Afront <3943720+Afront@users.noreply.github.com>
2020-03-25 23:30:22 +0900

    Add conditional for control characters

--- src/kibi.cr
+++ src/kibi.cr
@@ -6,6 +6,11 @@ module Kibi
 
   STDIN.raw do
     while (input_char = STDIN.read_char) && input_char != 'q'
+      if input_char.control?
+        puts input_char.ord
+      else
+        puts "#{input_char.ord} #{input_char}"
+      end
     end
   end
 end
